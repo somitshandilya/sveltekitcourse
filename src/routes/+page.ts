3c665baf2dd80b21aadc9a31ae924618239b5595
somitshandilya <somit@drupaltechie.com>
2024-08-09 16:56:36 +0530

    sveltekit-routing

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,7 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 // turns app into a SPA
-export const ssr = false
+export const ssr = true
 
 // turns app into SSG
 export const prerender = true
--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
   import { onDestroy, onMount } from 'svelte'
 
-  onMount(() => console.log('Mounted home page'))
-  onDestroy(() => console.log('Unmounted home page'))
+  onMount(() => console.log('Mounted posts page'))
+  onDestroy(() => console.log('Unmounted posts page'))
 </script>
 
 <!-- ... -->
-
 
 <nav>
   <a href="/">Home</a>
   <a href="/posts">Posts</a>
 </nav>
-<h1>Home page</h1>
+
+<h1>Posts page</h1>
